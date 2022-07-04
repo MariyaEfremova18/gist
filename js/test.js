0aad026b120b4843d71159463372a1693aa4a178
MariyaEfremova18 <108662400+MariyaEfremova18@users.noreply.github.com>
2022-07-04 18:37:14 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,93 +2,86 @@
 const arr = [2, -5, -4, 0, 5, 3, 2, 7, 1, 6, 3, -2, 0, 1];
 const arr1 = [1, 2, 3];
 
-// //Сделайте из него массив, состоящий из квадратов этих чисел
-// function squaring () {
-//    let arrSquared =  arr.map(value => Math.pow(value, 2));
-//    console.log(arrSquared);
-// };
+//Сделайте из него массив, состоящий из квадратов этих чисел
+function squaring () {
+   let arrSquared =  arr.map(value => Math.pow(value, 2));
+   console.log(arrSquared);
+};
 
-// squaring (arr);
+squaring (arr);
 
-// //Верните массив, состоящий только из уникальных значений(убрать все дубликаты, число в новом массиве не должно повторяться)
-// function returnUniqueValues () {
-//     let arrUnique = [];
-//     arr.forEach(item => {
-//         if (!arrUnique.includes(item)) {
-//             arrUnique.push(item);
-//         }
-//     });
-//     console.log(arrUnique);
-// };
+//Верните массив, состоящий только из уникальных значений(убрать все дубликаты, число в новом массиве не должно повторяться)
+function returnUniqueValues () {
+    let arrUnique = [];
+    arr.forEach(item => {
+        if (!arrUnique.includes(item)) {
+            arrUnique.push(item);
+        }
+    });
+    console.log(arrUnique);
+};
 
-// returnUniqueValues (arr);
+returnUniqueValues (arr);
 
-// //Проверьте то, что все элементы в массиве больше нуля (результат - true/false)
-// function findPositiveNumbers () {
-//     let result = '';
-//     for (let item of arr) {
-//         if (item == 0 || item < 0) {
-//             result = false;
-//             break;
-//         } else result = true;
-//     }
-//     console.log(result);
-// };
+//Проверьте то, что все элементы в массиве больше нуля (результат - true/false)
+function findPositiveNumbers () {
+    let result = '';
+    for (let item of arr) {
+        if (item == 0 || item < 0) {
+            result = false;
+            break;
+        } else result = true;
+    }
+    console.log(result);
+};
 
-// findPositiveNumbers (arr); 
+findPositiveNumbers (arr); 
 
-// // Оставьте в массиве только отрицательные числа
-// function returnNegativeValues () {
-//     let arrNegativeValues = [];
-//     arr.forEach(value => {
-//         if (value < 0)
-//         arrNegativeValues.push(value);
-//     });
-//     console.log(arrNegativeValues);
-// };
+// Оставьте в массиве только отрицательные числа
+function returnNegativeValues () {
+    let arrNegativeValues = [];
+    arr.forEach(value => {
+        if (value < 0)
+        arrNegativeValues.push(value);
+    });
+    console.log(arrNegativeValues);
+};
 
-// returnNegativeValues (arr);
+returnNegativeValues (arr);
 
-// // Найдите сумму элементов массива
-// function returnSum () {
-//     let sum = arr1.reduce((initial, item) => initial + item, 0);
-//     console.log(sum);
-// }
+// Найдите сумму элементов массива
+function returnSum () {
+    let sum = arr1.reduce((initial, item) => initial + item, 0);
+    console.log(sum);
+}
 
-// returnSum ();
-
-
-// // Дан массив, в нем могут быть обычные элементы и подмассивы(например [1, 2, [3, 4], 5, {a: 7}])
-// const arrWithSub = [1, 2, [3, 4], 5, {a: 7}];
-
-// //  Оставьте в нем только подмассивы
-// function returnSubarrays () {
-//     let result = arrWithSub.find((item) => Array.isArray(item));
-//     console.log(result);
-// };
-
-// returnSubarrays (arrWithSub);
+returnSum ();
 
 
-// // Верните количество слов в строке, длина которых больше 4 символов
-// let string = 'Съешь еще этих французских булок и выпей чаю';
+// Дан массив, в нем могут быть обычные элементы и подмассивы(например [1, 2, [3, 4], 5, {a: 7}])
+const arrWithSub = [1, 2, [3, 4], 5, {a: 7}];
 
-// function returnLongWords () {
-//     let arrString = string.split(' ');
-//     let sum = 0;
-//     arrString.forEach(item => {
-//         let result = item.split('');
-//         if (result.length > 4)
-//         sum += 1;
-//     })
-//     console.log(sum);
-// }
+//  Оставьте в нем только подмассивы
+function returnSubarrays () {
+    let result = arrWithSub.find((item) => Array.isArray(item));
+    console.log(result);
+};
 
-// returnLongWords (string);
+returnSubarrays (arrWithSub);
 
-//Если в строке имеется шестизначное число(ни больше, ни меньше) - верните его, как результат функции. Число может быть не отделено от других символов пробелами. 
-let str1 = 'abcd123456ef5234';
-let str2 ='abcef12345678ghi654321klm';
-let str3 ='abcdef';
-let str4 ='abc1234567asd';
-let str5 ='abc12345asd';
+
+// Верните количество слов в строке, длина которых больше 4 символов
+let string = 'Съешь еще этих французских булок и выпей чаю';
+
+function returnLongWords () {
+    let arrString = string.split(' ');
+    let sum = 0;
+    arrString.forEach(item => {
+        let result = item.split('');
+        if (result.length > 4)
+        sum += 1;
+    })
+    console.log(sum);
+}
+
+returnLongWords (string);
